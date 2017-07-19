5e84325851bbf6ab8b76d83a70d6df4d66856c5f
Krzysztof Jan Modras <chrmod@chrmod.net>
2017-07-19 19:04:30 +0200

    Simple typescript compiler options

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-export default class {
+import TypescriptCompiler from 'broccoli-typescript-compiler';
 
-}
+export default {
+  build(inputTree) {
+    return TypescriptCompiler.typescript(inputTree, {
+      tsconfig: {
+        compilerOptions: {
+          // TODO: check which options are required
+          module: 'commonjs',
+          target: 'es5',
+          moduleResolution: 'node',
+          newLine: 'LF',
+          sourceMap: true,
+          declaration: true,
+        },
+      },
+    });
+  },
+};
