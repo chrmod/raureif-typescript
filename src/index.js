2f6e3eb2b38fc2a1dbf48965bcf1a8a75c546f4d
Krzysztof Jan Modras <chrmod@chrmod.net>
2017-08-07 21:40:27 +0200

    Optimizing for raureif-glimmer

--- src/index.js
+++ src/index.js
@@ -1,24 +1,54 @@
 import TypescriptCompiler from 'broccoli-typescript-compiler';
 import Funnel from 'broccoli-funnel';
 import MergeTrees from 'broccoli-merge-trees';
+import path from 'path';
+import Source from 'broccoli-source';
+
+const UnwatchedDir = Source.UnwatchedDir;
 
 export default {
   exclude: ['**/*.ts'],
 
-  build(inputTree) {
+  build(inputTree, project) {
     const tmpDir = 'ts';
-    const tsTree = TypescriptCompiler.typescript(inputTree, {
+    const nodeModulesPath = path.join(project.path, 'node_modules');
+    const nodeTree = new UnwatchedDir(nodeModulesPath);
+    const input = new MergeTrees([
+      new Funnel(nodeTree, {
+        destDir: 'node_modules',
+      }),
+      new Funnel(nodeTree, {
+        srcDir: 'typescript/lib',
+      }),
+      new Funnel(inputTree, {
+        destDir: 'src',
+      }),
+    ]);
+
+    const tsTree = TypescriptCompiler.typescript(input, {
       tsconfig: {
         compilerOptions: {
           target: 'es5',
+          module: 'commonjs',
           moduleResolution: 'node',
+          experimentalDecorators: true,
+          noUnusedLocals: true,
           newLine: 'LF',
           inlineSourceMap: true,
           inlineSources: true,
           declaration: false,
           outDir: tmpDir,
           allowJs: true,
+          types: ['node'],
+          typeRoots: [path.join('node_modules', '@types')],
+          lib: [
+            'es2015',
+            'dom',
+          ],
         },
+        include: [
+          'src/**/*.ts',
+        ],
       },
     });
 
